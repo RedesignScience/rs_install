f319e922c8ea2fd41aa12124dd2da3f057a5e61c
Bosco Ho <apposite@gmail.com>
2022-07-18 14:29:05 +1000

    added file checks

--- rs_install_mac.py
+++ rs_install_mac.py
@@ -17,10 +17,10 @@ def run(cmds):
     return result
 
 
-def get_output_of_run(cmd):
+def get_lines_of_run(cmd):
     bytes = subprocess.check_output(cmd.split())
     text = bytes.decode("utf-8", errors="ignore")
-    return text
+    return text.splitlines()
 
 
 def install_cmd(binary, install_cmd):
@@ -33,11 +33,55 @@ def install_cmd(binary, install_cmd):
 
 def print_header(s):
     print()
+    print()
     print(">" * 70)
     for line in s.splitlines():
         print(f">   {line}")
     print(">" * 70)
 
+
+def get_home():
+    return  Path(os.path.expanduser("~"))
+
+
+def dirty_read_toml(fname):
+    result = {}
+    for line in open(fname):
+        if "=" in (tokens := line.split()):
+            result[tokens[0]] = tokens[2]
+    return result
+
+
+def check_aws_configure():
+    fname = get_home() / ".aws" / "credentials"
+    if not fname.exists():
+        return False
+    credentials = dirty_read_toml(fname)
+    if "aws_access_key_id" not in credentials:
+        return False
+    if "aws_secret_access_key" not in credentials:
+        return False
+    fname = get_home() / ".aws" / "config"
+    if not fname.exists():
+        return False
+    config = dirty_read_toml(fname)
+    if "region" not in config:
+        return False
+    if config["region"] != "us-east-1":
+        return False
+    return True
+
+
+def check_home_config_file(rel_home_fname, basename, url):
+    fname = get_home() / rel_home_fname
+    if fname.exists():
+        print(f"Check `{rel_home_fname}`: exists")
+    else:
+        print(f"Missing config: `{rel_home_fname}`")
+        print(f"Please download the file: `{basename}`")
+        print(f"From: {url}")
+        print(f"And copy to: `{fname}`")
+        print(f"")
 
 
 args = sys.argv[1:]
@@ -64,7 +108,9 @@ install_cmd(
     f'/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"'
 )
 install_cmd("wget", f"brew install wget")
-
+install_cmd("kubectl", f"brew install kubectl")
+install_cmd("argo", f"brew install argo")
+install_cmd("aws", f"brew install awscli")
 
 
 # Check for Conda install
@@ -89,10 +135,8 @@ if not shutil.which("conda"):
 print_header(f"Checking `{env}` conda environment")
 if run(f"conda env list | grep {env}").returncode != 0:
     run(f"conda create -n {env} -y python=3.8")
-text = get_output_of_run("conda env list")
-for line in text.splitlines():
-    tokens = line.split()
-    if env in tokens:
+for line in get_lines_of_run("conda env list"):
+    if env in (tokens := line.split()):
         env_path = tokens[-1]
         print(f"Location of `{env}` environment: {env_path}")
         break
@@ -105,7 +149,6 @@ else:
 print_header("Connect to R_S github website")
 install_cmd("gh", f"brew install gh")
 run("gh auth login")
-install_cmd("git", f"brew install git")
 
 
 # Checking for R_S package directory
@@ -120,33 +163,71 @@ print_header(f"R_S package: {package}")
 if not Path(package).exists():
     run(f"gh repo clone RedesignScience/{package}")
 os.chdir(package)
-run("git pull")
+run("gh repo sync")
 os.chdir('..')
 
 
 # Check for special Conda downloads 
-print_header("Conda install for special packages")
+print_header("Conda install for precompiled binaries")
 install_cmd("mamba", "conda install -y mamba")
-run(f"mamba env update -n {env} -f rs_install/rs_mac_conda_env.yaml")
+run(f"mamba env update -n {env} -f rs_install/mac_env.yaml")
 
 
 # Download R_S packages
 pip = f"{env_path}/bin/pip"
 for line in open("rs_install/packages.yaml"):
     tokens = line.split()
-    if len(tokens) and tokens[0] == "-":
-        package = tokens[1]
-        print_header(f"R_S package: {package}")
-        if not Path(package).exists():
-            run(f"gh repo clone RedesignScience/{package}")
-        os.chdir(package)
-        run(f"git pull")
-        if package == "foamdb":
-            run(f"{pip} install sqlalchemy alembic psycopg2-binary")
-            run(f"{pip} install .")
-        else:
-            run(f"{pip} install -e .")
-        os.chdir('..')
+    if not len(tokens) or tokens[0] != "-":
+        continue
+    package = tokens[1]
+    print_header(f"R_S package: {package}")
+    if not Path(package).exists():
+        run(f"gh repo clone RedesignScience/{package}")
+    os.chdir(package)
+    run(f"gh repo sync")
+    if package == "foamdb":
+        run(f"{pip} install sqlalchemy alembic psycopg2-binary")
+        run(f"{pip} install .")
+    else:
+        run(f"{pip} install -e .")
+    os.chdir('..')
+
+
+# Checking config file for tools that access our distributed platforms
+
+# Check AWS is configured
+print_header(f"Check AWS config: for launching remote jobs")
+if check_aws_configure():
+    print(f"AWS looks to be configured correctly")
+else:
+    print(f"")
+    print(f"You need to configure AWS with your credentials provided by our dev ops")
+    print(f"  - AWS Access Key ID: ?")
+    print(f"  - AWS Secret Access Key: ?")
+    print(f"  - region: us-east-1")
+    run("aws configure")
+
+
+# Check kubernetes to allow argo list
+print_header("Checking Kubernetes config: for monitoring remote jobs")
+check_home_config_file(
+    ".kube/config",
+    "cw-kubeconfig.txt",
+    "https://www.notion.so/redesignscience/Using-RSCloud-with-Orion-dd12c4c6e1d3473ab44678e0a05461b8#af8e52a7fcd14b298a1d7b081fae72b1"
+)
+
+# Check FoamDB to allow upload/downloand/viewing of trajectories
+print_header("Checking FoamDB config: for upload/download to traj database")
+check_home_config_file(
+    ".config/foamdb/config.json",
+    "config.json",
+    "https://www.notion.so/redesignscience/Credentials-55cd7fdaae764d699b1766ceafdd4dc9#74256ba09c404df899117022b225359d"
+)
+check_home_config_file(
+    ".hscfg",
+    ".hscfg",
+    "https://www.notion.so/redesignscience/Credentials-55cd7fdaae764d699b1766ceafdd4dc9#257f20440928466a862d225bb45405c6"
+)
 
 
 # Final instructions
@@ -158,7 +239,7 @@ The R_S packages should be in the directory
     {Path(".").resolve()}
 
 To access the packages in the `{env}` conda environment.
-
+d
     > conda activate {env}
     
 Check if `rseed` is available (you might have to wait for the Rosetta
